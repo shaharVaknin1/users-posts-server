7c153d920bbaa5a4c9f84da9c475a2cfc7283bb7
Shahar Vaknin <shaharvaknin909@gamil.com0>
2023-07-09 16:14:49 +0300

    add type to users from db

---
+++ src/db/users/types.ts
@@ -0,0 +1,17 @@
+interface DBUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  lat: string;
+  lng: string;
+  phone: string;
+  website: string;
+  company_name: string;
+  catch_phrase: string;
+  bs: string;
+}
